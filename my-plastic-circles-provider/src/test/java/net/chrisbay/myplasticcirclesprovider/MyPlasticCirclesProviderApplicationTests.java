c722e2065429d8cb8d8cc3dd01660052e44b736a
Chris Bay <chris@chrisbay.net>
2023-02-13 13:21:46 -0600

    Re-organizes project to use two separate modules

---
+++ my-plastic-circles-provider/src/test/java/net/chrisbay/myplasticcirclesprovider/MyPlasticCirclesProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chrisbay.myplasticcirclesprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlasticCirclesProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
