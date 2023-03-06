120b0a376eb1e370d2bcd7e18feaac508303b25b
Chris Bay <chris@chrisbay.net>
2023-03-06 15:56:28 -0600

    Removes test code

--- my-plastic-circles-angular/src/app/message-center/message-center.component.ts
+++ my-plastic-circles-angular/src/app/message-center/message-center.component.ts
@@ -17,11 +17,6 @@ export class MessageCenterComponent implements OnInit {
     this.messagesService.getMessages().subscribe({
       next: messages => this.messages = messages
     });
-
-    this.messagesService.addMessage({ type: "info", text: "Informational message"});
-    this.messagesService.addMessage({ type: "success", text: "It worked!"});
-    this.messagesService.addMessage({ type: "warning", text: "You should be aware of this"});
-    this.messagesService.addMessage({ type: "error", text: "Something very bad happened!"});
   }
 
   getIconClass(type: string) {
